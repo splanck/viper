ad7a92261a94e836738c1ffffdea496c3e74e4cd
Stephen Planck <splanck9271@gmail.com>
2025-09-12 20:16:21 -0400

    fix(vm): trap on missing runtime arguments
    RuntimeBridge::call now verifies argument counts for each runtime dispatch, issuing a descriptive trap when too few arguments are supplied. Added tests exercising under-supplied calls to ensure traps are raised rather than crashes.\n\n- [x] Built on Linux.\n- [x] ctest passed.\n- [x] Added/updated tests.\n- [x] Code formatted; no new warnings.\n- [x] No spec/API deviation (or ADR attached).\n- [ ] Docs updated if behavior/API changed.\n- [x] Updated docs/comments per policy.

--- src/vm/RuntimeBridge.cpp
+++ src/vm/RuntimeBridge.cpp
@@ -54,145 +54,191 @@ Slot RuntimeBridge::call(const std::string &name,
     curFn = fn;
     curBlock = block;
     Slot res{};
+    auto checkArgs = [&](size_t count)
+    {
+        if (args.size() < count)
+        {
+            std::ostringstream os;
+            os << name << ": expected " << count << " argument(s), got " << args.size();
+            RuntimeBridge::trap(os.str(), loc, fn, block);
+            return false;
+        }
+        return true;
+    };
     if (name == "rt_print_str")
     {
-        rt_print_str(args[0].str);
+        if (checkArgs(1))
+            rt_print_str(args[0].str);
     }
     else if (name == "rt_print_i64")
     {
-        rt_print_i64(args[0].i64);
+        if (checkArgs(1))
+            rt_print_i64(args[0].i64);
     }
     else if (name == "rt_print_f64")
     {
-        rt_print_f64(args[0].f64);
+        if (checkArgs(1))
+            rt_print_f64(args[0].f64);
     }
     else if (name == "rt_len")
     {
-        res.i64 = rt_len(args[0].str);
+        if (checkArgs(1))
+            res.i64 = rt_len(args[0].str);
     }
     else if (name == "rt_concat")
     {
-        res.str = rt_concat(args[0].str, args[1].str);
+        if (checkArgs(2))
+            res.str = rt_concat(args[0].str, args[1].str);
     }
     else if (name == "rt_substr")
     {
-        res.str = rt_substr(args[0].str, args[1].i64, args[2].i64);
+        if (checkArgs(3))
+            res.str = rt_substr(args[0].str, args[1].i64, args[2].i64);
     }
     else if (name == "rt_str_eq")
     {
-        res.i64 = rt_str_eq(args[0].str, args[1].str);
+        if (checkArgs(2))
+            res.i64 = rt_str_eq(args[0].str, args[1].str);
     }
     else if (name == "rt_input_line")
     {
-        res.str = rt_input_line();
+        if (checkArgs(0))
+            res.str = rt_input_line();
     }
     else if (name == "rt_to_int")
     {
-        res.i64 = rt_to_int(args[0].str);
+        if (checkArgs(1))
+            res.i64 = rt_to_int(args[0].str);
     }
     else if (name == "rt_int_to_str")
     {
-        res.str = rt_int_to_str(args[0].i64);
+        if (checkArgs(1))
+            res.str = rt_int_to_str(args[0].i64);
     }
     else if (name == "rt_f64_to_str")
     {
-        res.str = rt_f64_to_str(args[0].f64);
+        if (checkArgs(1))
+            res.str = rt_f64_to_str(args[0].f64);
     }
     else if (name == "rt_alloc")
     {
-        res.ptr = rt_alloc(args[0].i64);
+        if (checkArgs(1))
+            res.ptr = rt_alloc(args[0].i64);
     }
     else if (name == "rt_left")
     {
-        res.str = rt_left(args[0].str, args[1].i64);
+        if (checkArgs(2))
+            res.str = rt_left(args[0].str, args[1].i64);
     }
     else if (name == "rt_right")
     {
-        res.str = rt_right(args[0].str, args[1].i64);
+        if (checkArgs(2))
+            res.str = rt_right(args[0].str, args[1].i64);
     }
     else if (name == "rt_mid2")
     {
-        res.str = rt_mid2(args[0].str, args[1].i64);
+        if (checkArgs(2))
+            res.str = rt_mid2(args[0].str, args[1].i64);
     }
     else if (name == "rt_mid3")
     {
-        res.str = rt_mid3(args[0].str, args[1].i64, args[2].i64);
+        if (checkArgs(3))
+            res.str = rt_mid3(args[0].str, args[1].i64, args[2].i64);
     }
     else if (name == "rt_instr2")
     {
-        res.i64 = rt_instr2(args[0].str, args[1].str);
+        if (checkArgs(2))
+            res.i64 = rt_instr2(args[0].str, args[1].str);
     }
     else if (name == "rt_instr3")
     {
-        res.i64 = rt_instr3(args[0].i64, args[1].str, args[2].str);
+        if (checkArgs(3))
+            res.i64 = rt_instr3(args[0].i64, args[1].str, args[2].str);
     }
     else if (name == "rt_ltrim")
     {
-        res.str = rt_ltrim(args[0].str);
+        if (checkArgs(1))
+            res.str = rt_ltrim(args[0].str);
     }
     else if (name == "rt_rtrim")
     {
-        res.str = rt_rtrim(args[0].str);
+        if (checkArgs(1))
+            res.str = rt_rtrim(args[0].str);
     }
     else if (name == "rt_trim")
     {
-        res.str = rt_trim(args[0].str);
+        if (checkArgs(1))
+            res.str = rt_trim(args[0].str);
     }
     else if (name == "rt_ucase")
     {
-        res.str = rt_ucase(args[0].str);
+        if (checkArgs(1))
+            res.str = rt_ucase(args[0].str);
     }
     else if (name == "rt_lcase")
     {
-        res.str = rt_lcase(args[0].str);
+        if (checkArgs(1))
+            res.str = rt_lcase(args[0].str);
     }
     else if (name == "rt_chr")
     {
-        res.str = rt_chr(args[0].i64);
+        if (checkArgs(1))
+            res.str = rt_chr(args[0].i64);
     }
     else if (name == "rt_asc")
     {
-        res.i64 = rt_asc(args[0].str);
+        if (checkArgs(1))
+            res.i64 = rt_asc(args[0].str);
     }
     else if (name == "rt_sqrt")
     {
-        res.f64 = rt_sqrt(args[0].f64);
+        if (checkArgs(1))
+            res.f64 = rt_sqrt(args[0].f64);
     }
     else if (name == "rt_floor")
     {
-        res.f64 = rt_floor(args[0].f64);
+        if (checkArgs(1))
+            res.f64 = rt_floor(args[0].f64);
     }
     else if (name == "rt_ceil")
     {
-        res.f64 = rt_ceil(args[0].f64);
+        if (checkArgs(1))
+            res.f64 = rt_ceil(args[0].f64);
     }
     else if (name == "rt_sin")
     {
-        res.f64 = rt_sin(args[0].f64);
+        if (checkArgs(1))
+            res.f64 = rt_sin(args[0].f64);
     }
     else if (name == "rt_cos")
     {
-        res.f64 = rt_cos(args[0].f64);
+        if (checkArgs(1))
+            res.f64 = rt_cos(args[0].f64);
     }
     else if (name == "rt_pow")
     {
-        res.f64 = rt_pow(args[0].f64, args[1].f64);
+        if (checkArgs(2))
+            res.f64 = rt_pow(args[0].f64, args[1].f64);
     }
     else if (name == "rt_abs_i64")
     {
-        res.i64 = rt_abs_i64(args[0].i64);
+        if (checkArgs(1))
+            res.i64 = rt_abs_i64(args[0].i64);
     }
     else if (name == "rt_abs_f64")
     {
-        res.f64 = rt_abs_f64(args[0].f64);
+        if (checkArgs(1))
+            res.f64 = rt_abs_f64(args[0].f64);
     }
     else if (name == "rt_randomize_i64")
     {
-        rt_randomize_i64(args[0].i64);
+        if (checkArgs(1))
+            rt_randomize_i64(args[0].i64);
     }
     else if (name == "rt_rnd")
     {
-        res.f64 = rt_rnd();
+        if (checkArgs(0))
+            res.f64 = rt_rnd();
     }
     else
     {
