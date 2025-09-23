ce74560b344f25f36664975c80bc4c737e97c21b
Stephen Planck <splanck9271@gmail.com>
2025-09-23 10:36:05 -0400

    refactor(frontends/basic): inline Lowerer helper declarations

--- src/frontends/basic/LowerStmt.cpp
+++ src/frontends/basic/LowerStmt.cpp
@@ -6,6 +6,7 @@
 // Ownership/Lifetime: Operates on Lowerer state without owning AST or module.
 // Links: docs/class-catalog.md
 
+// Requires the consolidated Lowerer interface for statement lowering helpers.
 #include "frontends/basic/Lowerer.hpp"
 #include "il/core/BasicBlock.hpp"
 #include "il/core/Function.hpp"
