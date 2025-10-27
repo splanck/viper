6d4b643d1e2b9e94d5e25fbd113207b5f1d80619
Stephen Planck <splanck9271@gmail.com>
2025-10-27 17:07:00 -0400

    fix(vm): report active block for switch traps

--- src/vm/ops/Op_BranchSwitch.cpp
+++ src/vm/ops/Op_BranchSwitch.cpp
@@ -230,7 +230,7 @@ VM::ExecResult handleSwitchI32(VM &vm,
                                size_t &ip)
 {
     (void)blocks;
-    (void)bb;
+    
     (void)ip;
 
     const Slot scrutineeSlot = VMAccess::eval(vm, fr, switchScrutinee(in));
@@ -306,8 +306,7 @@ VM::ExecResult handleSwitchI32(VM &vm,
                             "switch target out of range",
                             in.loc,
                             fr.func ? fr.func->name : std::string(),
-                            fr.func && !fr.func->blocks.empty() ? fr.func->blocks.front().label
-                                                                : "");
+                            bb ? bb->label : std::string());
         return result;
     }
 
